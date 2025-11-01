2cdf5dab1a7b345ffff45b2604c57351e853ca9b
LemoxEGG <lemoxaxolotl@proton.me>
2025-11-01 19:23:27 -0400

    replaced flake with custom one for packaging the server removed result directory from tracking

--- flake.nix
+++ flake.nix
@@ -1,38 +1,45 @@
 {
+  description = "Rust development shell template, can be used with 'nix develop' or 'nix shell'";
+
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      utils,
-      naersk,
-    }:
-    utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell =
-          with pkgs;
-          mkShell {
-            buildInputs = [
-              cargo
-              rustc
-              rustfmt
-              pre-commit
-              rustPackages.clippy
-              rust-analyzer
-            ];
-            RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          };
-      }
-    );
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    rec {
+      dependencies = with pkgs; [
+        rustc
+        cargo
+        gcc
+        rustfmt
+        clippy
+        rust-analyzer
+      ];
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = dependencies;
+        shellHook = "echo 'Rust shell init complete.'";
+      };
+
+      default = pkgs.rustPlatform.buildRustPackage rec {
+        pname = "rocket-webserver";
+        version = "0.1.0";
+        src = pkgs.lib.cleanSource ./.;
+        nativeBuildInputs = dependencies;
+
+        cargoLock.lockFile = "${src}/Cargo.lock";
+        # phases = [ "buildPhase" "installPhase" ];
+        # buildPhase = "cargo build --release";
+        # installPhase = "cp $src/target/release/file_server $out/bin/fileserver";
+      };
+
+      packages.${system}.default = default;
+
+      RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    };
 }
