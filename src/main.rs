1274ea179290e33e16ac7cd287d2dcb1515c0597
LemoxEGG <lemoxaxolotl@proton.me>
2025-11-01 18:12:46 -0400

    changed website host directory

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use rocket::{fs::FileServer, launch};
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", FileServer::from("/www"))
+    rocket::build().mount("/", FileServer::from("/ssd/www"))
 }
