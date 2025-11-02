bccfcddca431f00ab7e9c0b16a16f2daea1f80ff
LemoxEGG <lemoxaxolotl@proton.me>
2025-11-02 13:54:10 -0500

    changed routing logic to redirect to en directory by default

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
-use rocket::{fs::FileServer, launch};
+use std::path::{Path, PathBuf};
+
+use rocket::{
+    fs::{FileServer, NamedFile},
+    get, launch,
+    response::Redirect,
+    routes, uri,
+};
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", FileServer::from("/ssd/www"))
+    rocket::build().mount("/", routes![default, en])
+}
+
+#[get("/<uri..>")]
+async fn en(uri: PathBuf) -> Option<NamedFile> {
+    let uri = dbg!(uri);
+
+    let uri = Path::new("/ssd/www").join(uri);
+    if uri.is_dir() {
+        NamedFile::open(uri.join("index.html")).await.ok()
+    } else {
+        NamedFile::open(uri).await.ok()
+    }
+}
+
+#[get("/")]
+fn default() -> Redirect {
+    Redirect::to(uri!(en(Path::new("/en"))))
 }
