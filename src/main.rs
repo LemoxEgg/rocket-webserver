abd8b1c2ff9a4f2a42f5ccbb0b46c19a68af4eab
LemoxEGG <lemoxaxolotl@proton.me>
2025-11-15 16:59:37 -0500

    added more routes to reflect the ui changes

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 use std::path::{Path, PathBuf};
 
-use rocket::{
-    fs::{FileServer, NamedFile},
-    get, launch,
-    response::Redirect,
-    routes, uri,
-};
+use rocket::{fs::NamedFile, get, launch, response::Redirect, routes, uri};
+
+const DEFAULT_PATH: &str = "/ssd/www";
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![default, en])
+    rocket::build().mount(
+        "/",
+        routes![default, files, server_en, server_fr, server_tok],
+    )
 }
 
 #[get("/<uri..>")]
-async fn en(uri: PathBuf) -> Option<NamedFile> {
+async fn files(uri: PathBuf) -> Option<NamedFile> {
     let uri = dbg!(uri);
 
-    let uri = Path::new("/ssd/www").join(uri);
+    let uri = Path::new(DEFAULT_PATH).join(uri);
     if uri.is_dir() {
         NamedFile::open(uri.join("index.html")).await.ok()
     } else {
@@ -24,7 +24,32 @@ async fn en(uri: PathBuf) -> Option<NamedFile> {
     }
 }
 
+#[get("/en/<file>")]
+async fn server_en(file: String) -> Option<NamedFile> {
+    NamedFile::open(dbg!(
+        Path::new(DEFAULT_PATH).join("en").join(file + ".html")
+    ))
+    .await
+    .ok()
+}
+#[get("/fr/<file>")]
+async fn server_fr(file: String) -> Option<NamedFile> {
+    NamedFile::open(dbg!(
+        Path::new(DEFAULT_PATH).join("fr").join(file + ".html")
+    ))
+    .await
+    .ok()
+}
+#[get("/tok/<file>")]
+async fn server_tok(file: String) -> Option<NamedFile> {
+    NamedFile::open(dbg!(
+        Path::new(DEFAULT_PATH).join("tok").join(file + ".html")
+    ))
+    .await
+    .ok()
+}
+
 #[get("/")]
 fn default() -> Redirect {
-    Redirect::to(uri!(en(Path::new("/en"))))
+    Redirect::to(uri!(files(Path::new("/en"))))
 }
